bd50e46ed594678ddbf3a3335de08ee43eb88613
darwin939 <askaruly@hotmail.com>
2020-07-04 00:29:31 +0600

    cors

--- nateste/settings.py
+++ nateste/settings.py
@@ -118,3 +118,16 @@ STATIC_URL = '/static/'
 
 
 
+#l
+CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'https://thawing-reef-32246.herokuapp.com',
+    "http://localhost:5000"
+] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
+CORS_ORIGIN_REGEX_WHITELIST = [
+    'http://localhost:3000',
+    'https://thawing-reef-32246.herokuapp.com/api/order/',
+    "http://localhost:5000"
+]
