0fc0d80eff52e9fabc8b507ad860110b91fac2a0
darwin939 <askaruly@hotmail.com>
2020-07-04 13:59:00 +0600

    add static

--- nateste/settings.py
+++ nateste/settings.py
@@ -114,8 +114,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 
 
 #lines to enable cors anywhere
