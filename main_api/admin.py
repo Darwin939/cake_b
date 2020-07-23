9443e19f2f53da97b8cff8562b4460e003e9f940
darwin939 <askaruly@hotmail.com>
2020-07-24 00:58:22 +0600

    register avatar in admin panel

--- main_api/admin.py
+++ main_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Review, Profile,Order
+from .models import Review, Profile,Order,Avatar
 
 admin.site.register(Profile)
 admin.site.register(Order)
 admin.site.register(Review)
+admin.site.register(Avatar)
