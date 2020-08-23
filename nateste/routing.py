2a23134dcd601e63b11a2e537b76d42eb27b2ec7
darwin939 <askaruly@hotmail.com>
2020-08-23 20:03:19 +0600

    consum

--- nateste/routing.py
+++ nateste/routing.py
@@ -1,12 +1,10 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.sessions import SessionMiddlewareStack
-
 import chat.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': SessionMiddlewareStack(
+    'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
