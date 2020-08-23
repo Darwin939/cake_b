36de52a570943b6bc59de98323a0608e967fc6cb
darwin939 <askaruly@hotmail.com>
2020-08-23 19:56:40 +0600

    consum

--- chat/routing.py
+++ chat/routing.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import  path ,re_path
 
 from . import consumers
@@ -7,3 +8,7 @@ websocket_urlpatterns = [
     re_path(r'ws/chat/', consumers.ChatConsumer), #(?P<room_name>\w+)/$
     # re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
 ]
+
+
+
+
--- nateste/routing.py
+++ nateste/routing.py
@@ -1,10 +1,12 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.sessions import SessionMiddlewareStack
+
 import chat.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
+    'websocket': SessionMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
